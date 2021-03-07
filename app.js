84db3feb06f63d8199e2239713e02273811ad84f
lukegammon <54638816+lukegammon@users.noreply.github.com>
2021-03-07 21:54:09 +0000

    update

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const hamburgerMenu = document.querySelector(".hamburger");
+const navDropdown = document.querySelector(".nav__dropdown");
+const overlay = document.querySelector(".overlay");
 
 hamburgerMenu.addEventListener("click", () => {
     if(hamburgerMenu.classList.contains("hamburger-active")) {
         hamburgerMenu.classList.remove("hamburger-active");
+        navDropdown.style.display = "none";
+        overlay.style.display = "none";
     } else {
         hamburgerMenu.classList.add("hamburger-active");
+        navDropdown.style.display = "block";
+        overlay.style.display = "block";     
     }
-})
+});
