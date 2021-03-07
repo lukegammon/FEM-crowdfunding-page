60b7f595b661a801cb5f96225bc6161cdb30a8c5
lukegammon <54638816+lukegammon@users.noreply.github.com>
2021-03-07 22:51:59 +0000

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const hamburgerMenu = document.querySelector(".hamburger");
 const navDropdown = document.querySelector(".nav__dropdown");
 const overlay = document.querySelector(".overlay");
 
+
 hamburgerMenu.addEventListener("click", () => {
     if(hamburgerMenu.classList.contains("hamburger-active")) {
         hamburgerMenu.classList.remove("hamburger-active");
@@ -13,3 +14,18 @@ hamburgerMenu.addEventListener("click", () => {
         overlay.style.display = "block";     
     }
 });
+
+const bookmarkButton = document.querySelector(".product__cta-bookmark");
+
+bookmarkButton.addEventListener("click", () => {
+    if(bookmarkButton.classList.contains("product__cta-bookmark-active")){
+        bookmarkButton.classList.add("product__cta-bookmark");
+        bookmarkButton.classList.remove("product__cta-bookmark-active");
+    } else {
+        bookmarkButton.classList.add("product__cta-bookmark-active");
+        bookmarkButton.classList.remove("product__cta-bookmark");
+    }
+});
+
+
+
